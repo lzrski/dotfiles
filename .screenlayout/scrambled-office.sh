630c3e84a3df415d6e20e276079d02e0bb4b42e2
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-05-22 10:10:00 +0200

    Add dual display configuration for Scrambled Office
    Using .xprofile (executed when loging in to X session) and
    .screenlayouts/scrambled-office.sh (generated with arandr with
    additional dpi setting).

---
+++ .screenlayout/scrambled-office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --dpi 144 --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP2 --off
