75c0b6da26eb6c15822df9fb623d365fd873965e
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-06-01 16:09:30 +0200

    Do not load Gnome Settings Daemon
    In recent Gnome it was split into multiple small daemons and I couldn't
    get it to work properly. Loading gsd-xsettings was affecting Gnome apps,
    but breaking Chromium and Atom. Without it Gnome apps were tiny (96 DPI).
    
    Now instead I use Xresources to control DPI of the apps that don't honour
    xrandr --dpi option.
    
    The inspiration came from:
    
    https://www.reddit.com/r/i3wm/comments/4iu24f/dpi_problems_with_hidpi_in_gnome_apps/
    
    Also format the code in .screenlayout/scrambled-office.sh so it's more
    readable.

--- .screenlayout/scrambled-office.sh
+++ .screenlayout/scrambled-office.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-xrandr --dpi 144 --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP2 --off
+xrandr \
+  --dpi 144 \
+  --output eDP-1 \
+    --mode 1920x1080 \
+    --pos 0x1080 \
+    --rotate normal \
+  --output HDMI-1 \
+    --mode 3840x2160 \
+    --pos 1920x0 \
+    --rotate normal \
+  --output DP-2 --off \
+  --output DP-1 --off \
+  --output HDMI-3 --off \
+  --output HDMI-2 --off \
