91289d342d9c694dd0ba455ef8514fde6c6645dc
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-06-19 14:04:29 +0200

    Change output names in screen layout (xrandr command)
    Again. Why is this happening? Seems like it switched after starting
    Wayland. Hmm...

--- .screenlayout/scrambled-office.sh
+++ .screenlayout/scrambled-office.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 xrandr \
   --dpi 144 \
-  --output eDP-1 \
+  --output eDP1 \
     --mode 1920x1080 \
     --pos 0x1080 \
     --rotate normal \
-  --output HDMI-1 \
+  --output HDMI1 \
     --mode 3840x2160 \
     --pos 1920x0 \
     --rotate normal \
-  --output DP-2 --off \
-  --output DP-1 --off \
-  --output HDMI-3 --off \
-  --output HDMI-2 --off \
+
